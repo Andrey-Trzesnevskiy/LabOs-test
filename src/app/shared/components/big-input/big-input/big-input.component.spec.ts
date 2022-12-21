99730064d01387daee3ae4d852dd2b247bc540ca
Andrii.Trzhesnevskyi <Andrii.Trzhesnevskyi.ext@dkv-mobility.com>
2022-12-21 14:25:18 +0200

    test fixes

--- src/app/shared/components/big-input/big-input/big-input.component.spec.ts
+++ src/app/shared/components/big-input/big-input/big-input.component.spec.ts
@@ -1,8 +1,7 @@
 import { Component, DebugElement } from '@angular/core';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { SharedModule } from '../../shared.module';
+import { SharedModule } from 'app/shared/shared.module';
 
 @Component({
   selector: 'st-host-for-test',
